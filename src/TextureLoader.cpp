a07540bdfbe47adab3341b1f57c25a447c8de555
shresthasameerman <ssameer23@tbc.edu.np>
2026-03-05 13:22:21 +0545

    player ship rotates to face mouse

---
+++ src/TextureLoader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sameer on 3/5/26.
+//
+
+#include "TextureLoader.h"
---
+++ src/TextureLoader.h
@@ -0,0 +1,13 @@
+//
+// Created by sameer on 3/5/26.
+//
+
+#ifndef VOIDSHOOTER_TEXTURELOADER_H
+#define VOIDSHOOTER_TEXTURELOADER_H
+
+
+class TextureLoader {
+};
+
+
+#endif //VOIDSHOOTER_TEXTURELOADER_H
